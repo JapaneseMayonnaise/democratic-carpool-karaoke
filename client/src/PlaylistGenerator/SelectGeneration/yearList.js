84388b07f603ae81fb7f1697ebb79f21fc0bca0e
DrCardamom <erkshimba@gmail.com>
2020-05-29 17:16:38 -0400

    re-order birthyear picker form to descedant and removed 2021 to 2029

--- client/src/PlaylistGenerator/SelectGeneration/yearList.js
+++ client/src/PlaylistGenerator/SelectGeneration/yearList.js
@@ -25,18 +25,27 @@
   * from 1900 to 2020.
   * 
   * decadeArray looks like 
-  *   [{decade: '1900'}, {decade: '1910'},... {decade: '2020'}]
+  *   [ {decade: '1900'}, 
+  *     {decade: '1910'},
+  *      ... 
+  *     {decade: '2020'}
+  *    ]
   * 
   */
 
 const generateYearList = () => {
-   const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-   const anotherNum = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
+   const num = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+   const anotherNum = [120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0];
    
    const decadeArray = anotherNum.map(eachNum => ({ decade: (1900 + eachNum).toString()}));
+
    const clonedArray = decadeArray.map(element => {
-      const yearBorn = num.map(eachNum => (parseInt(element.decade) + eachNum).toString())
-      return {...element, yearBorn};
+      if(element.decade !== '2020'){
+         const yearBorn = num.map(eachNum => (parseInt(element.decade) + eachNum).toString())
+         return {...element, yearBorn};
+      } else {
+         return {...element, yearBorn: ['2020']};
+      }  
    });
 
    return clonedArray;
