d59fb162fef33f6f770c411ae098ded4bb9bf94f
DrCardamom <erkshimba@gmail.com>
2020-06-02 09:48:22 -0400

    add map function in mer.js to not use for-loop

--- controllers/secondClick/merge.js
+++ controllers/secondClick/merge.js
@@ -5,25 +5,14 @@ const sharedVar = require('../../config/sharedVariables');
 *  Every 2 songs extracted from each trackIdArray.
 */
 const mix2Arrays = () => {
-
-  console.log('@mix2aarays before sharedVar.mixedArray', sharedVar.mixedArray);
-  console.log('@mix2aarays before sharedVar.trackIdArray_User1', sharedVar.trackIdArray_User1);
-  console.log('@mix2aarays before sharedVar.trackIdArray_User2', sharedVar.trackIdArray_User2);
-
-    let whereToInsert = 2;
-    for(let i = 0; i < sharedVar.trackIdArray_User1.length; i++)
-    {
-      sharedVar.trackIdArray_User2.splice(whereToInsert, 0, sharedVar.trackIdArray_User1[i], sharedVar.trackIdArray_User1[i + 1]);
-      whereToInsert = whereToInsert + 4;
-      i++;
-    }
-
-    sharedVar.mixedArray = sharedVar.trackIdArray_User2;
-
-    console.log('@mix2aarays after sharedVar.mixedArray', sharedVar.mixedArray);
-    console.log('@mix2aarays after sharedVar.trackIdArray_User1', sharedVar.trackIdArray_User1);
-    console.log('@mix2aarays after  sharedVar.trackIdArray_User2', sharedVar.trackIdArray_User2);
-    
+    sharedVar.mixedArray = sharedVar.trackIdArray_User1.flatMap((trackId_user1, i) => {
+      if(i % 2 === 1) {
+            return [trackId_user1, sharedVar.trackIdArray_User2[i-1], sharedVar.trackIdArray_User2[i]]
+          } else {
+            return trackId_user1;
+          }
+      }  
+    ); 
 }
 
 module.exports = {
